9677f8a4ab0e198dba43b46496ab55bd28b30c81
A-CHINMAY <a.chinmay2005@gmail.com>
2025-01-18 21:32:58 +0530

    added

--- module4/insertBST.c
+++ module4/insertBST.c
@@ -1,4 +1,27 @@
+void insert_node(TreePointer *node, int num) {
+    
+    TreePointer ptr, temp = modified_search(*node, num);
 
+    if (temp || !(*node)) {
+        ptr = (TreePointer)malloc(sizeof(TreeNode));
 
+        if (ptr == NULL) {
+            fprintf(stderr, "The memory is full\n");
+            exit(1);
+        }
+        ptr->data = num;
+        ptr->left = NULL;
+        ptr->right = NULL;
 
-
+        if (*node) {
+            /* insert as child of temp */
+            if (num < temp->data)
+                temp->left = ptr;
+            else
+                temp->right = ptr;
+        }
+        else {
+            *node = ptr;
+        }
+    }
+}
